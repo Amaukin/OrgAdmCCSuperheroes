f099eaf39abf32c7859e9da5bc80386c9e157b0a
amauri.pineda <amauri.pineda@netlogistik.com>
2022-06-06 22:32:43 -0500

    Router de api de superheroes

---
+++ routes/superheroesApi.js
@@ -0,0 +1,132 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Superheroe = require('../models/superheroes');
+
+const ERROR_ENCONTRADO = 'Hubo un error';
+const SUPERHEROE_ELIMINADO = 'Se ha eliminado al superheroe';
+const SUPERHEROE_NO_EXISTE = 'No se ha encontrado el superheroe';
+const SUPERHEROE_YA_EXISTE = 'Ese superheroe ya está registrado';
+
+// BORRA ESTE COMMENT Y LO SUBE JOSFER
+/* GET superheroes listing. */
+router.get('/', function(req, res, next) {
+  Superheroe.find({}, function(err, superheroes) {
+    res.json(superheroes);
+  })
+});
+
+// BORRA ESTE COMMENT Y LO SUBE DANIEL
+/* POST superheroe. */
+router.post('/', async function (req, res, next) {
+  console.log('heroe', req);
+  var nombreHeroe = req.body.nombre;
+  var nombreHeroeUpper = nombreHeroe.toUpperCase();
+  const superheroeEncontrado = await Superheroe.findOne({ nombre: nombreHeroeUpper });
+  if (superheroeEncontrado) {
+    res.send(SUPERHEROE_YA_EXISTE);
+    res.send(SUPERHEROE_YA_EXISTE);
+  } else {
+    req.body = sanitizarSuperheroe(req.body);
+    var superheroe = Superheroe({
+      _id: mongoose.Types.ObjectId(),
+      nombre: req.body.nombre,
+      alineacion: req.body.alineacion,
+      equipo: req.body.equipo,
+      superpoderes: req.body.superpoderes,
+      imagen: req.body.imagen,
+      franquicia: req.body.franquicia
+    });
+    superheroe.save((err, superheroe) => {
+      if (err) {
+        console.log(ERROR_ENCONTRADO, err); 
+      } else {
+        res.redirect('../superheroes');
+      }
+    });
+  }
+});
+
+// BORRA ESTE COMMENT Y LO SUBE MANUEL
+/* PATCH superheroe to update. */
+router.patch('/:id', async function (req, res, next) {
+  var superheroeId = req.params.id;
+  var superheroeEncontrado = await Superheroe.findOne({ _id: superheroeId });
+  if (superheroeEncontrado) {
+    if (req.body.nombre) superheroeEncontrado.nombre = req.body.nombre;
+    if (req.body.alineacion) superheroeEncontrado.alineacion = req.body.alineacion;
+    if (req.body.equipo) superheroeEncontrado.equipo = req.body.equipo;
+    if (req.body.superpoderes) superheroeEncontrado.superpoderes = req.body.superpoderes;
+    if (req.body.imagen) superheroeEncontrado.imagen = req.body.imagen;
+    if (req.body.franquicia) superheroeEncontrado.franquicia = req.body.franquicia;
+
+    await superheroeEncontrado.save();
+
+    res.send(superheroeEncontrado);
+  } else {
+    res.send(404, SUPERHEROE_NO_EXISTE);
+  }
+});
+
+// BORRA ESTE COMMENT Y LO SUBE EFRAIN
+/* PATCH superheroe to update. */
+router.post('/:id', async function (req, res, next) {
+  console.log('vava', req.query);
+  if (req.query.isDelete) {
+    console.log('se eliminaría');
+    Superheroe.findByIdAndDelete(req.params.id, (err) => {
+      if (err) {
+        res.send(500, ERROR_ENCONTRADO + err);
+      } else {
+        res.redirect('../superheroes');
+      }
+    })
+  } else if (req.query.isEdit) {
+    var superheroeEditado = await Superheroe.findOne({ _id: req.params.id });
+    console.log('encontrado', superheroeEditado);
+    req.body = sanitizarSuperheroe(req.body);
+    superheroeEditado.nombre = req.body.nombre;
+    superheroeEditado.alineacion = req.body.alineacion;
+    superheroeEditado.equipo = req.body.equipo;
+    superheroeEditado.superpoderes = req.body.superpoderes;
+    superheroeEditado.imagen = req.body.imagen;
+    superheroeEditado.franquicia = req.body.franquicia;
+    superheroeEditado.save((err, superheroe) => {
+      if (err) {
+        console.log(500, ERROR_ENCONTRADO + err);
+      } else {
+        res.redirect('../superheroes');
+      }
+    });
+  }
+});
+
+// BORRA ESTE COMMENT Y LO SUBE EFRAIN
+/* Delete superheroe. */
+router.delete('/:id', async function (req, res, next) {
+  Superheroe.findByIdAndDelete(req.body.id, (err) =>{
+    if (err) {
+      res.send(500, ERROR_ENCONTRADO + err);
+    } else {
+      res.send(SUPERHEROE_ELIMINADO);
+    }
+  })
+});
+
+function sanitizarSuperheroe(reqBody) {
+  if (reqBody.equipo === '') delete reqBody.equipo;
+  var superpoderesNull = true 
+  reqBody.superpoderes.forEach((superpoder, index) => {
+    if (superpoder === '') {
+      reqBody.superpoderes.splice(index, 1);
+    } else {
+      superpoderesNull = false;
+    }
+  });
+  if (superpoderesNull) delete reqBody.superpoderes;
+  if (reqBody.imagen === '') delete reqBody.imagen;
+  if (reqBody.franquicia === '') delete reqBody.franquicia;
+  return reqBody
+}
+
+module.exports = router;
