3ef65cdf5435092c82c8e2d3c4c233c04cf30754
josfer <fernandovillavicencio56@gmial.com>
2022-06-07 22:29:33 -0500

    index.js y layout.hbs terminados

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Superheroe = require('../models/superheroes');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Superheroe.count().exec(async function (err, count) {
+    var randomNumber = Math.floor(Math.random() * count)
+    var randomSuperheroe = await Superheroe.findOne().skip(randomNumber);
+    res.render('index', { title: 'SuperAPI', randomSuperheroe: randomSuperheroe });
+  })
+});
+
+/* GET home page. */
+router.get('/home', function (req, res, next) {
+  Superheroe.count().exec(async function (err, count) {
+    var randomNumber = Math.floor(Math.random() * count)
+    var randomSuperheroe = await Superheroe.findOne().skip(randomNumber);
+    res.render('index', { title: 'SuperAPI', randomSuperheroe: randomSuperheroe });
+  })
 });
 
 module.exports = router;
